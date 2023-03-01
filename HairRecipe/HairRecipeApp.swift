e17111340007ed4b95690fa06bbb013e6ca6e073
Andrey91 <mistaandrey@icloud.com>
2023-03-01 11:50:39 +0500

    Initiate project

---
+++ HairRecipe/HairRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HairRecipeApp.swift
+//  HairRecipe
+//
+//  Created by Andrey Piskunov on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HairRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
