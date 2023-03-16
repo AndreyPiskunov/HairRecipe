1446b587aa8fb901e10d52a1f6a5b7c3b0faa9a4
Andrey91 <mistaandrey@icloud.com>
2023-03-16 11:06:40 +0500

    Minor fixes for focus on fields

---
+++ HairRecipe/Model/FocusOnFields.swift
@@ -0,0 +1,15 @@
+//
+//  FocusOnFields.swift
+//  HairRecipe
+//
+//  Created by Andrey Piskunov on 15.03.2023.
+//
+
+import Foundation
+
+enum FocusOnFields: Hashable {
+    case nameField
+    case procedureField
+    case priceField
+    case recipeField
+}
--- HairRecipe/Views/CreateClient/CreateClientView.swift
+++ HairRecipe/Views/CreateClient/CreateClientView.swift
@@ -7,49 +7,52 @@
 
 import SwiftUI
 
-enum FocusOnFields: Hashable {
-    case nameField
-    case procedureField
-    case priceField
-    case recipeField
-}
-
 struct CreateClientView: View {
     
     @Environment(\.dismiss) private var dismiss
     @ObservedObject var viewModel: EditClientViewModel
     
     @State private var hasError: Bool = false
-    
     @FocusState private var fieldFocus: FocusOnFields?
+    
     
     var body: some View {
         List {
             Section("Biography") {
                 TextField ("Name", text: $viewModel.client.name)
-                    .keyboardType(.namePhonePad)
+                    .submitLabel(.next)
                     .focused($fieldFocus, equals: .nameField)
                 
                 TextField ("Procedure", text: $viewModel.client.procedure)
+                    .submitLabel(.next)
                     .focused($fieldFocus, equals: .procedureField)
                 
-                DatePicker("Date", selection: $viewModel.client.date,
-                           displayedComponents: [.date])
-                .datePickerStyle(.compact)
+                DatePicker("Date", selection: $viewModel.client.date, displayedComponents: [.date])
+                    .datePickerStyle(.compact)
                 
                 TextField ("Price", text: $viewModel.client.price)
-                    .keyboardType(.phonePad)
+                    .keyboardType(.numberPad)
                     .focused($fieldFocus, equals: .priceField)
             }
             Section("Recipe") {
-                TextField ("Add recipe",
+                TextField ("Enter recipe or note",
                            text: $viewModel.client.recipe,
                            axis: .vertical)
                 .focused($fieldFocus, equals: .recipeField)
             }
-        }.onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                fieldFocus = .nameField
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    fieldFocus = .nameField
+                }
+            }
+        }.onSubmit {
+            switch fieldFocus {
+            case .nameField:
+                fieldFocus = .procedureField
+            case .procedureField:
+                fieldFocus = .priceField
+            default:
+                fieldFocus = nil
             }
         }
         .navigationTitle(viewModel.isNewClient ? "New Client" : "Edit Client")
@@ -99,7 +102,6 @@ private extension CreateClientView {
 struct CreateClientView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            
             let preview = ClientsProvider.shared
             
             CreateClientView(viewModel: .init(provider: preview))
